b9ad963a35de94005f316413d6f37db746609873
Shobhit Chourasia <>
2021-06-13 18:54:12 +0530

    1. Initial commit.

---
+++ CustomPullTab/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CustomPullTab
+//
+//  Created by Shobhit on 13/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
